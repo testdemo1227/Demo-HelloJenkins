97bbecab4b37c33c6b1b584b4ea661710afdaaf3
testdemo1227 <testdemo1227@outlook.com>
2022-02-28 05:16:14 -0500

    New Commit
    Maven Project

---
+++ src/test/java/HelloJenkins_123/TestHelloJenkins.java
@@ -0,0 +1,13 @@
+package HelloJenkins_123;
+
+import org.testng.annotations.Test;
+
+public class TestHelloJenkins {
+	
+	@Test
+	public void TestHelloWorld() {
+		
+		System.out.printIn("... Hello Jenkins Project ...")
+	}
+
+}
